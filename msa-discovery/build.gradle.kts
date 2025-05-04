c0ee236859e77b33b559acbb545ebea75e2aeaf3
writer0713 <writer0713@naver.com>
2025-05-04 23:55:29 +0900

    feat: Eureka Server 설정 완료 (#1)
    - msa-discovery 유레카서버 설정 완료

--- msa-discovery/build.gradle.kts
+++ msa-discovery/build.gradle.kts
@@ -2,5 +2,15 @@ plugins {
     id("msa.kotlin-spring-convention")
 }
 
+extra["springCloudVersion"] = "2024.0.1"
+
 dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-server")
+}
+
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+    }
 }
