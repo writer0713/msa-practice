11535d743261c72a66fbc4182eb4ce31455a48af
writer0713 <writer0713@naver.com>
2025-05-06 23:54:14 +0900

    refactor: MainController RequestMapping 수정 (#3)
    - msa-first-service / msa-second-service maincontroller requestmapping 추가

--- msa-first-service/src/main/kotlin/com/writer0713/first/controller/MainController.kt
+++ msa-first-service/src/main/kotlin/com/writer0713/first/controller/MainController.kt
@@ -1,8 +1,10 @@
 package com.writer0713.first.controller
 
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
+@RequestMapping("/first-service")
 @RestController
 class MainController {
     @GetMapping("/")
--- msa-second-service/src/main/kotlin/com/writer0713/second/controller/MainController.kt
+++ msa-second-service/src/main/kotlin/com/writer0713/second/controller/MainController.kt
@@ -1,8 +1,10 @@
 package com.writer0713.second.controller
 
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
+@RequestMapping("/second-service")
 @RestController
 class MainController {
     @GetMapping("/")
