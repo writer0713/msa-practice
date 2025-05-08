b4bbf5d960ce7dae9e745a8ccb766308379779b1
writer0713 <writer0713@naver.com>
2025-05-08 23:13:18 +0900

    feat: yaml 파일 대신 config 파일로 routes 설정 (#7)

---
+++ msa-gateway/src/main/kotlin/com/writer0713/gateway/config/FilterConfig.kt
@@ -0,0 +1,39 @@
+package com.writer0713.gateway.config
+
+import org.springframework.cloud.gateway.route.RouteLocator
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder
+import org.springframework.cloud.gateway.route.builder.routes
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class FilterConfig {
+    @Bean
+    fun gatewayRoutes(builder: RouteLocatorBuilder): RouteLocator {
+        val routeLocator = builder.routes()
+
+        // first-service
+        routeLocator.route { r ->
+            r
+                .path("/first-service/**")
+                .filters {
+                    it
+                        .addRequestHeader("first-request", "first-request-header")
+                        .addResponseHeader("first-response", "first-response-header")
+                }.uri("http://127.0.0.1:8081")
+        }
+
+        // second-service
+        routeLocator.route { r ->
+            r
+                .path("/second-service/**")
+                .filters {
+                    it
+                        .addRequestHeader("second-request", "second-request-header")
+                        .addResponseHeader("second-response", "second-resposne-header")
+                }.uri("http://127.0.0.1:8082")
+        }
+
+        return routeLocator.build()
+    }
+}
