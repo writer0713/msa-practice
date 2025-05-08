fa7f97edb685a99f9afa1fc722353656ef2935be
writer0713 <writer0713@naver.com>
2025-05-08 23:45:21 +0900

    refactor: FilterConfig 사용 안하도록 수정 (#7)

--- msa-gateway/src/main/kotlin/com/writer0713/gateway/config/FilterConfig.kt
+++ msa-gateway/src/main/kotlin/com/writer0713/gateway/config/FilterConfig.kt
@@ -2,13 +2,10 @@ package com.writer0713.gateway.config
 
 import org.springframework.cloud.gateway.route.RouteLocator
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder
-import org.springframework.cloud.gateway.route.builder.routes
-import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Configuration
 
-@Configuration
+// @Configuration
 class FilterConfig {
-    @Bean
+    //    @Bean
     fun gatewayRoutes(builder: RouteLocatorBuilder): RouteLocator {
         val routeLocator = builder.routes()
 
