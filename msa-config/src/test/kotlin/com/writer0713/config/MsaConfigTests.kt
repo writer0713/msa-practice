ddb622c8d717e4e1a6f0543e903243d4dcc9b25d
writer0713 <writer0713@naver.com>
2025-06-06 15:30:14 +0900

    feat: [msa-config] config server 구축 (#31)
    - msa-user config 추가

---
+++ msa-config/src/test/kotlin/com/writer0713/config/MsaConfigTests.kt
@@ -0,0 +1,11 @@
+package com.writer0713.config
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MsaConfigTests {
+    @Test
+    fun contextLoads() {
+    }
+}
