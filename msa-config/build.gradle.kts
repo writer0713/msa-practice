a1ecad2e578e33ed71d0a61afe68c311ac7566e2
writer0713 <writer0713@naver.com>
2025-06-07 22:11:05 +0900

    feat: msa-config : docker-compose + rabbitmq 세팅 (#33)
    - docker-compose 로 rabbitmq 띄우도록 세팅
    - spring-boot-docker-compose 로 application 뜰때 자동으로 rabbitmq 띄우도록

--- msa-config/build.gradle.kts
+++ msa-config/build.gradle.kts
@@ -7,6 +7,9 @@ extra["springCloudVersion"] = "2024.0.1"
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.springframework.cloud:spring-cloud-config-server")
+
+    // 개발환경에서만 사용
+    developmentOnly("org.springframework.boot:spring-boot-docker-compose")
 }
 
 dependencyManagement {
