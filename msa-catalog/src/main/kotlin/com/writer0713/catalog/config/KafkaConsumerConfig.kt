c0827dfb1485c54a240a90c91b744e7390fc2e5f
writer0713 <writer0713@naver.com>
2025-06-25 17:54:37 +0900

    feat: msa-catalog - kafka consuming 구현 (#39)
    - kafka consuming 구현

---
+++ msa-catalog/src/main/kotlin/com/writer0713/catalog/config/KafkaConsumerConfig.kt
@@ -0,0 +1,32 @@
+package com.writer0713.catalog.config
+
+import org.apache.kafka.clients.consumer.ConsumerConfig
+import org.apache.kafka.common.serialization.StringDeserializer
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.kafka.annotation.EnableKafka
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory
+import org.springframework.kafka.core.ConsumerFactory
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory
+
+@EnableKafka
+@Configuration
+class KafkaConsumerConfig {
+    @Bean
+    fun consumerFactory(): ConsumerFactory<String, String> {
+        val properties = HashMap<String, Any>()
+        properties[ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG] = "127.0.0.1:9092"
+        properties[ConsumerConfig.GROUP_ID_CONFIG] = "consumer-group-id"
+        properties[ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG] = StringDeserializer::class.java
+        properties[ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG] = StringDeserializer::class.java
+
+        return DefaultKafkaConsumerFactory(properties)
+    }
+
+    @Bean
+    fun concurrentKafkaListenerContainerFactory(): ConcurrentKafkaListenerContainerFactory<String, String> {
+        val factory = ConcurrentKafkaListenerContainerFactory<String, String>()
+        factory.consumerFactory = consumerFactory()
+        return factory
+    }
+}
